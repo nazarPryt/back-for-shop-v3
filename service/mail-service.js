48aec09a02aaca98c6e96e0231807eac3625fbf4
nazarPtyt <ockap90@libero.it>
2023-05-09 21:55:23 +0200

    feat: add nodemail & configured MailService

--- service/mail-service.js
+++ service/mail-service.js
@@ -1,12 +1,32 @@
+const nodemailer = require('nodemailer')
+
 class MailService {
-  async sendActivationMail(to, link){
-    try{
-      console.log('re');
 
-
-    }catch (e) {
-      console.log(e);
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port:process.env.SMTP_PORT,
+      secure: false,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
     }
+    })
+  }
+  async sendActivationMail(to, link){
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: 'activation account on ' + process.env.API_URL,
+      text: '',
+      html:
+      `
+        <div>
+            <h1>For activation click on link</h1>
+            <a href="${link}">${link}</a>
+        </div>
+      `
+    })
   }
 
 }
