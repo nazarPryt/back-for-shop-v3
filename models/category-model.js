791f6052efe83970ff46834d90b5626f17927f65
nazarPtyt <ockap90@libero.it>
2023-05-16 19:00:21 +0200

    ~~~~~ delete all and start from scratch with typescript ~~~~

--- models/category-model.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const CategorySchema = new Schema({
-  name: { type: String, unique: true},
-  products: {type: Array},
-})
-module.exports = model('Category', CategorySchema)
--- models/order-model.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const OrderSchema = new Schema({
-  userID: {type: Schema.Types.ObjectId, ref: 'User'},
-  products: {type: Array},
-})
-
-module.exports = model('Order', OrderSchema)
